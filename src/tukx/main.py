ca984776fbb1cfe30092a538f94ffd16fd486c93
Jakub Szulc <58491724+j-szulc@users.noreply.github.com>
2023-10-22 19:31:26 +0200

    Big commit, setup arg parsing, jinja rendering

--- src/tukx/main.py
+++ src/tukx/main.py
@@ -1,6 +1,11 @@
-import argparse
 import logging
 import sys
+import uuid
+import os
+import shlex
+import shutil
+import pathlib
+import jinja2
 
 import click
 
@@ -11,6 +16,18 @@ __copyright__ = "Jakub Szulc"
 __license__ = "MIT"
 
 _logger = logging.getLogger(__name__)
+
+__jinja2_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(str(pathlib.Path(__file__).parent / "templates")),
+    autoescape=jinja2.select_autoescape(),
+    lstrip_blocks=True,
+    trim_blocks=True
+)
+
+__templates = {
+    "permanent": "permanent.service.j2",
+    "inline-file": "inline-file.sh.j2",
+}
 
 
 def setup_logging(loglevel):
@@ -25,14 +42,101 @@ def setup_logging(loglevel):
     )
 
 
+def get_cmd_list(command, shell=False):
+    command = [line.strip() for line in command.splitlines() if line.strip()]
+    if not command:
+        return []
+    if shell:
+        return ["sh", "-c", " ; ".join(command)]
+    if len(command) != 1:
+        raise click.ClickException("Only one command can be specified when shell is disabled.")
+    return shlex.split(command[0])
+
+
+def fix_exec_path(cmd_list):
+    if not cmd_list:
+        return []
+    if shutil.which(cmd_list[0]) is None:
+        raise click.ClickException("Command '{}' not found in PATH".format(cmd_list[0]))
+    cmd_list[0] = shutil.which(cmd_list[0])
+    return cmd_list
+
+
+def fix_envlist(envlist):
+    for arg in envlist:
+        # Note: that's a very rudimentary check, do not rely on it.
+        if "=" not in arg:
+            raise click.ClickException("Invalid environment variable: {}. Format: NAME=VALUE".format(arg))
+        name, value = arg.split("=", 1)
+        if not all(c in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_" for c in name):
+            raise click.ClickException("Invalid environment variable name: {}".format(name))
+        if not value.isprintable():
+            raise click.ClickException("Invalid environment variable value: {}".format(value))
+        yield "{}={}".format(name, value)
+
 @click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def main(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo(f"Hello {name}!")
+@click.option("--dry-run", is_flag=True, help="Only print generated command to stdout and nothing else.")
+@click.option("--verbose", is_flag=True, help="Enables verbose mode.")
+@click.option("--permanent/--transient", default=False, show_default=True, help="If transient, use systemd-run and remove the service after logout.")
+@click.option("--description", default=None)
+@click.option("--unit", default=None, help="Name of the service (Default: random)")
+@click.option("--user", help="Run service as user (Default: current user)")
+@click.option("--group", help="Run service as group (Default: current user's group)")
+@click.option("--restart", default="no", show_default=True, help="Restart policy")
+@click.option("--working-directory", default=None, help="Working directory (Default: current directory)")
+@click.option("--environment", "-E", default=[], multiple=True, help="Environment variables", metavar="NAME=VALUE")
+@click.option("--system-wide/--user-wide", default=True, show_default=True, help="Install service system-wide or user-wide")
+@click.option("--shell", is_flag=True,  help="Run the command in a shell.")
+@click.option("--install/--dont-install", default=True, show_default=True, help="Install the service")
+@click.argument("command", nargs=-1)
+def main(dry_run, verbose, permanent, description, unit, user, group, restart, working_directory, environment, system_wide, shell, install, command):
+    """
+    tukx - Run commands as systemd services
+
+    COMMAND - Command to run as a service.
+              If not specified, the command will be read from stdin.
+              Press Ctrl+D to finish input.
+    """
+    print(str(pathlib.Path(__file__).parent / "templates"))
+    unit="tukx-{}".format(uuid.uuid4()) if unit is None else unit
+    if verbose:
+        setup_logging(logging.DEBUG)
+    working_directory = working_directory if working_directory is not None else "."
+    working_directory = os.path.abspath(working_directory)
+    if not os.path.isdir(working_directory):
+        raise click.ClickException("Working directory '{}' does not exist".format(working_directory))
+    if user is None:
+        user = os.getlogin()
+    if group is None:
+        group = user
+    environment = fix_envlist(environment)
+    if not command:
+        print("Enter command to run as a service. Press Ctrl+D to finish input.", file=sys.stderr)
+        command = sys.stdin.read()
+    if isinstance(command, tuple):
+        command = " ".join(command)
+    command = shlex.join(fix_exec_path(get_cmd_list(command, shell)))
+    if not command:
+        raise click.ClickException("No command specified")
+
+    template_file = __templates["permanent"] if permanent else __templates["transient"]
+    kwargs = {
+        "description": description,
+        "unit": unit,
+        "user": user,
+        "group": group,
+        "working_directory": working_directory,
+        "environment": environment,
+        "command": command,
+        "restart": restart,
+        "install": install,
+    }
+    print(__jinja2_env.get_template(template_file).render(**kwargs))
+
+
+
+
+
 
 
 if __name__ == '__main__':
